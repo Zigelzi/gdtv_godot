5ee61e2840e23bcb31c8b338820c2d40656c0882
zigelzi <miika.a.savela@gmail.com>
2024-06-22 09:31:41 +0300

    Add destroying enemies with rockets

--- alien_attack/scripts/enemy.gd
+++ alien_attack/scripts/enemy.gd
@@ -3,13 +3,16 @@ extends Area2D
 @export var _speed: float = 200.0
 
 func _ready():
-    $VisibleNotifier.screen_exited.connect(_on_enemy_screen_exited)
+	$VisibleNotifier.screen_exited.connect(_on_enemy_screen_exited)
 
 func _physics_process(delta):
-    global_position.x -= _speed * delta
+	global_position.x -= _speed * delta
 
 func _on_enemy_screen_exited():
-    _destroy_offscreen()
+	_destroy_offscreen()
 
 func _destroy_offscreen() -> void:
-    queue_free()
+	queue_free()
+
+func die():
+	queue_free()
--- alien_attack/scripts/rocket.gd
+++ alien_attack/scripts/rocket.gd
@@ -4,12 +4,18 @@ extends Area2D
 
 func _ready():
 	$VisibleNotifier.screen_exited.connect(_on_rocket_screen_exited)
+	$".".area_entered.connect(_on_enemy_area_entered)
 
 func _physics_process(delta):
 	global_position.x += _speed * delta
 
-func _on_rocket_screen_exited():
+func _on_rocket_screen_exited() -> void:
 	_destroy_offscreen()
 
-func _destroy_offscreen():
+func _destroy_offscreen() -> void:
 	queue_free()
+
+func _on_enemy_area_entered(area: Area2D) -> void:
+	if area.has_method("die"):
+		area.die()
+		queue_free()
