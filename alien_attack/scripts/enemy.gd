db47f20614bbc3f6ff7706a68915bddc94371954
zigelzi <miika.a.savela@gmail.com>
2024-06-23 15:04:44 +0300

    Draft of adding player lives

--- alien_attack/scripts/enemy.gd
+++ alien_attack/scripts/enemy.gd
@@ -1,9 +1,12 @@
 extends Area2D
 
+signal player_hit
+
 @export var _speed: float = 200.0
 
 func _ready():
 	$VisibleNotifier.screen_exited.connect(_on_enemy_screen_exited)
+	$".".body_entered.connect(_on_player_body_entered)
 
 func _physics_process(delta):
 	global_position.x -= _speed * delta
@@ -13,6 +16,9 @@ func _on_enemy_screen_exited():
 
 func _destroy_offscreen() -> void:
 	queue_free()
+
+func _on_player_body_entered(_body: Node2D):
+	die()
 
 func die():
 	queue_free()
---
+++ alien_attack/scripts/game.gd
@@ -0,0 +1,6 @@
+extends Node2D
+
+@export var _lives: int = 3
+
+func _ready():
+    pass
