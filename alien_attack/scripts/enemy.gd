485f87554bbb1b2dfeb43342717ff793ab75f26b
Miika Savela <miika.a.savela@gmail.com>
2024-06-24 19:48:10 +0300

    Add stop spawning enemies when game ends

--- alien_attack/scripts/enemy.gd
+++ alien_attack/scripts/enemy.gd
@@ -5,22 +5,19 @@ signal player_hit
 @export var _speed: float = 200.0
 
 func _ready():
-	$VisibleNotifier.screen_exited.connect(_on_enemy_screen_exited)
 	$".".body_entered.connect(_on_player_body_entered)
+	$VisibleNotifier.screen_exited.connect(_on_enemy_screen_exited)
 
 func _physics_process(delta):
 	global_position.x -= _speed * delta
-
-func _on_enemy_screen_exited():
-	_destroy_offscreen()
-
-func _destroy_offscreen() -> void:
-	queue_free()
 
 func _on_player_body_entered(body: Node2D):
 	if (body.has_method("take_damage")):
 		body.take_damage()
 	die()
 
-func die():
+func _on_enemy_screen_exited():
+	die()
+
+func die() -> void:
 	queue_free()
--- alien_attack/scripts/enemy_spawner.gd
+++ alien_attack/scripts/enemy_spawner.gd
@@ -1,17 +1,23 @@
 extends Node2D
 
-@onready var _spawn_positions: Array[Node] = $SpawnPositions.get_children()
+@onready var _root_node: Node2D = $".".get_node("/root/Game") as Node2D
 @onready var _spawned_enemies: Node = $Enemies
+@onready var _spawn_positions: Array[Node] = $SpawnPositions.get_children()
+@onready var _spawn_timer: Timer = $SpawnTimer
 
 var _enemy: PackedScene = preload ("res://scenes/enemy_1.tscn")
 
 func _ready():
-    $SpawnTimer.timeout.connect(_on_spawn_timer_timeout)
+	_spawn_timer.timeout.connect(_on_spawn_timer_timeout)
+	_root_node.game_ended.connect(_on_game_ended)
 
 func _on_spawn_timer_timeout():
-    _spawn_enemy_to_random_spawn_point()
+	_spawn_enemy_to_random_spawn_point()
 
 func _spawn_enemy_to_random_spawn_point() -> void:
-    var enemy_instance: Node = _enemy.instantiate()
-    _spawned_enemies.add_child(enemy_instance)
-    enemy_instance.global_position = _spawn_positions.pick_random().global_position
+	var enemy_instance: Node = _enemy.instantiate()
+	_spawned_enemies.add_child(enemy_instance)
+	enemy_instance.global_position = _spawn_positions.pick_random().global_position
+
+func _on_game_ended():
+	_spawn_timer.stop()
