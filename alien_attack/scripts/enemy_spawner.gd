4299ec4b7a9fab759694b772dc1883b28889666f
Miika Savela <miika.a.savela@gmail.com>
2024-06-30 10:15:43 +0300

    Add enemy moving along path and adjust enemy spawning to have multiple enemy types

--- alien_attack/scripts/enemy_spawner.gd
+++ alien_attack/scripts/enemy_spawner.gd
@@ -2,12 +2,12 @@ extends Node2D
 
 signal enemy_spawned(new_enemy: Node)
 
+@export var _enemy_types: Array[PackedScene]
+
 @onready var _root_node: Node2D = $".".get_node("/root/Game") as Node2D
 @onready var _spawned_enemies: Node = $Enemies
 @onready var _spawn_positions: Array[Node] = $SpawnPositions.get_children()
 @onready var _spawn_timer: Timer = $SpawnTimer
-
-var _enemy: PackedScene = preload ("res://scenes/enemy_1.tscn")
 
 func _ready():
 	_spawn_timer.timeout.connect(_on_spawn_timer_timeout)
@@ -17,7 +17,8 @@ func _on_spawn_timer_timeout() -> void:
 	_spawn_enemy_to_random_spawn_point()
 
 func _spawn_enemy_to_random_spawn_point() -> void:
-	var enemy_instance: Node = _enemy.instantiate()
+	if _enemy_types.is_empty(): return
+	var enemy_instance: Node = _enemy_types[0].instantiate()
 	enemy_spawned.emit(enemy_instance)
 	_spawned_enemies.add_child(enemy_instance)
 	enemy_instance.global_position = _spawn_positions.pick_random().global_position
