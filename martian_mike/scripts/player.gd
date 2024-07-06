0e95b1c6b0a13ee9728a37292399b77d91e383a9
Miika Savela <miika.a.savela@gmail.com>
2024-07-06 16:29:31 +0300

    Add player being affected by gravity

--- martian_mike/scripts/player.gd
+++ martian_mike/scripts/player.gd
@@ -1,1 +1,11 @@
 extends CharacterBody2D
+
+@export var speed: int = 200
+
+@onready var default_gravity: float = ProjectSettings.get_setting("physics/2d/default_gravity")
+
+func _physics_process(delta):
+    if !is_on_floor():
+        velocity.y += default_gravity * delta
+    
+    move_and_slide()
