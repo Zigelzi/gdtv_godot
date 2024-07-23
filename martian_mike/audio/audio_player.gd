d3083849a4e7d44c7b05422e39ed9854cd8556f3
zigelzi <miika.a.savela@gmail.com>
2024-07-23 09:59:55 +0300

    Adjust first level to be more dangerous

--- martian_mike/audio/audio_player.gd
+++ martian_mike/audio/audio_player.gd
@@ -1,30 +1,30 @@
 extends Node
 class_name GameAudioPlayer
 
-var _hurt: AudioStreamWAV = preload ("res://assets/audio/hurt.wav")
-var _jump: AudioStreamWAV = preload ("res://assets/audio/jump.wav")
+var _hurt: AudioStreamWAV = preload ("./hurt.wav")
+var _jump: AudioStreamWAV = preload ("./jump.wav")
 
 func _ready():
-    disable_music_in_debug()
-    
+	disable_music_in_debug()
+	
 func play_sfx(sfx_name: String) -> void:
-    var sfx_player: AudioStreamPlayer = AudioStreamPlayer.new()
-    sfx_player.name = "SFX" + sfx_name
-    sfx_player.volume_db = -22
+	var sfx_player: AudioStreamPlayer = AudioStreamPlayer.new()
+	sfx_player.name = "SFX" + sfx_name
+	sfx_player.volume_db = -22
 
-    if sfx_name == "hurt":
-        sfx_player.stream = _hurt
-    elif sfx_name == "jump":
-        sfx_player.stream = _jump
-    
-    add_child(sfx_player)
-    sfx_player.play()
-    await sfx_player.finished
-    sfx_player.queue_free()
+	if sfx_name == "hurt":
+		sfx_player.stream = _hurt
+	elif sfx_name == "jump":
+		sfx_player.stream = _jump
+	
+	add_child(sfx_player)
+	sfx_player.play()
+	await sfx_player.finished
+	sfx_player.queue_free()
 
 func disable_music_in_debug():
-    if OS.is_debug_build():
-        $MusicPlayer.autoplay = false
-        $MusicPlayer.stop()
-    else:
-        $MusicPlayer.autoplay = true
+	if OS.is_debug_build():
+		$MusicPlayer.autoplay = false
+		$MusicPlayer.stop()
+	else:
+		$MusicPlayer.autoplay = true
