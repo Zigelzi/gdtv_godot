37ff9fdef68e651a7e1181c49b4c2015dc49dae7
Miika Savela <miika.a.savela@gmail.com>
2024-07-31 09:35:54 +0300

    Change movement to use acceleration and adjust jump pad positions

--- martian_mike/player/player.gd
+++ martian_mike/player/player.gd
@@ -2,7 +2,18 @@ extends CharacterBody2D
 class_name Player
 
 signal energy_updated(new_energy: float)
-@export var _speed: int = 200
+
+@export_subgroup("Movement")
+@export var _max_movement_speed: int = 200
+@export var _max_acceleration: float = 60.0
+
+@export_subgroup("Dashing")
+@export var _dash_velocity: float = 500.0
+
+@export_subgroup("Energy")
+@export var _max_energy: float = 100
+@export var _walking_energy_consumption: float = 5.0
+@export var _jump_energy_consumption: float = 5.0
 
 @export_subgroup("Jumping")
 @export var _max_fall_velocity: float = 600
@@ -10,30 +21,28 @@ signal energy_updated(new_energy: float)
 @export var _jump_distance_to_peak: float = 40.0 # px
 @export var _jump_distance_to_descent: float = 40.0 # px
 
-@export_subgroup("Energy")
-@export var _max_energy: float = 100
-@export var _walking_energy_consumption: float = 5.0
-@export var _jump_energy_consumption: float = 5.0
-
 @onready var _animations: AnimatedSprite2D = $AnimatedSprite2D
-@onready var _jump_velocity: float = ((2.0 * _jump_height * _speed) / _jump_distance_to_peak) * -1.0
-@onready var _jump_gravity: float = ((-2.0 * _jump_height * (_speed * _speed)) / (_jump_distance_to_peak * _jump_distance_to_peak)) * -1.0
-@onready var _fall_gravity: float = ((-2.0 * _jump_height * (_speed * _speed)) / (_jump_distance_to_descent * _jump_distance_to_descent)) * -1.0
+@onready var _jump_velocity: float = ((2.0 * _jump_height * _max_movement_speed) / _jump_distance_to_peak) * -1.0
+@onready var _jump_gravity: float = ((-2.0 * _jump_height * (_max_movement_speed * _max_movement_speed)) / (_jump_distance_to_peak * _jump_distance_to_peak)) * -1.0
+@onready var _fall_gravity: float = ((-2.0 * _jump_height * (_max_movement_speed * _max_movement_speed)) / (_jump_distance_to_descent * _jump_distance_to_descent)) * -1.0
 
 var _current_energy: float = -1.0
 var start_position: Vector2 = Vector2.ZERO
 var is_active: bool = true
+var _is_dashing: bool = false
+var _direction: int = 0
 
 func _ready():
 	_current_energy = _max_energy
 	energy_updated.emit(_current_energy)
 
 func _physics_process(delta):
-	var direction: float = 0
 	if is_active:
-		direction = _get_movement_input()
-		velocity.x = direction * _speed
+		_direction = _get_movement_input()
 		_get_jump_input()
+		_get_dash_input()
+
+		_move(delta)
 	else:
 		velocity.x = 0
 	
@@ -43,8 +52,8 @@ func _physics_process(delta):
 	if velocity.y >= _max_fall_velocity:
 		velocity.y = _max_fall_velocity
 
-	if direction != 0:
-		_animations.flip_h = direction == -1
+	if _direction != 0:
+		_animations.flip_h = _direction == -1
 		if is_on_floor():
 			_current_energy -= _walking_energy_consumption * delta
 			energy_updated.emit(_current_energy)
@@ -53,13 +62,18 @@ func _physics_process(delta):
 		_current_energy = 0
 		reset()
 
-	_update_animations(direction)
+	_update_animations(_direction)
 	move_and_slide()
 
-func _get_movement_input() -> float:
+func _get_movement_input() -> int:
 	var input: float = Input.get_axis("move_left", "move_right")
 	
-	return input
+	return int(input)
+
+func _move(delta: float) -> void:
+	var desired_velocity = _direction * _max_movement_speed
+	var max_speed_change = _max_acceleration * delta
+	velocity.x = move_toward(velocity.x, desired_velocity, max_speed_change)
 
 #region Jumping
 
@@ -108,4 +122,17 @@ func reset() -> void:
 func grant_energy(amount: float) -> void:
 	_current_energy = minf(_max_energy, _current_energy + amount)
 	energy_updated.emit(_current_energy)
+#endregion
+
+# region Dashing
+func _get_dash_input() -> void:
+	if Input.is_action_just_pressed("dash"):
+		_dash()
+
+func _dash() -> void:
+	_is_dashing = true
+	if _direction == 0:
+		velocity.x += _dash_velocity
+	else:
+		velocity.x += _dash_velocity * _direction
 #endregion
